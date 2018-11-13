fa24f74cb93f47ec95d00838af6de11672b82e1e
Ray Deck <ray@raydeck.com>
2018-11-13 13:09:38 -0500

    Making serverless-setup the central point of generating config information

--- cli.js
+++ cli.js
@@ -3,7 +3,31 @@ const commander = require("commander");
 const { makeConfig, writeConfig } = require("./");
 commander
   .description("Create config.json")
-  .action(async args => {
-    writeConfig(await makeConfig(args));
+  .option(
+    "-p --currentPath <pathname>",
+    "Reference path for examining a serverlesss setup"
+  )
+  .option("-f --file-name", "File to write output (default config.json)")
+  .option(
+    "-s --standard-output",
+    "Write to standard output instead of config.json"
+  )
+  .action(async ({ currentPath, targetPath, fileName, standardOutput }) => {
+    fileName = fileName && "config.json";
+    targetPath = targetPath && process.cwd();
+    try {
+      const c = await makeConfig({ currentPath });
+      if (standardOutput) {
+        console.log(JSON.stringify(c, null, 2));
+      } else {
+        writeConfig(c, targetPath, fileName);
+      }
+    } catch (e) {
+      console.log(
+        "Caught an error! Is it possible  upstream serverless stacks are not deployed yet?"
+      );
+      console.log("Error message:", e.toString());
+      process.exit(1);
+    }
   })
   .parse(process.argv);
--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 const { STS } = require("aws-sdk");
-const Path = require("path");
-const fs = require("fs");
+const { join, resolve, dirname } = require("path");
+const { readFileSync, writeFileSync, lstatSync, existsSync } = require("fs");
+const { getResources } = require("serverless-resources");
+let cachedAccountId = null;
 const getAccountID = async () => {
-  const { Account } = await new STS().getCallerIdentity().promise();
-  return Account;
+  if (!cachedAccountId) {
+    const { Account } = await new STS().getCallerIdentity().promise();
+    cachedAccountId = Account;
+  }
+  return cachedAccountId;
 };
-const makeConfig = async ({ currentPath = process.cwd() }) => {
+const makeConfig = async ({
+  currentPath = process.cwd(),
+  getMyResources,
+  ...cmd
+}) => {
   let awsAccountId = await getAccountID();
+  const oldPath = process.cwd();
+  process.chdir(currentPath);
   let o = { awsAccountId };
   //open the package.json\
-  const packagePath = Path.join(currentPath, "package.json");
-  if (fs.existsSync(packagePath)) {
-    const { name, version, serverless } = JSON.parse(
-      fs.readFileSync(packagePath, { encoding: "UTF8" })
+  const packagePath = join("package.json");
+  if (existsSync(packagePath)) {
+    let { name, version, serverless: rawServerless } = JSON.parse(
+      readFileSync(packagePath, { encoding: "UTF8" })
     );
-    o = { ...o, name, version, ...serverless };
+    o = { ...o, name, version };
+    if (rawServerless) {
+      let { dependencies, ...serverless } = rawServerless;
+      o = { ...o, ...serverless };
+      if (dependencies) {
+        for (let [k, path] of Object.entries(dependencies)) {
+          //Make sure its config is up to date
+          path = resolve(path);
+          const basePath = lstatSync(path).isDirectory ? path : dirname(path);
+          let config = await makeConfig({ ...cmd, currentPath: basePath });
+          if (!existsSync(join(basePath, "config.json")))
+            writeConfig(config, basePath);
+          let r = {
+            ...config,
+            ...(await getResources({ ...cmd, path: basePath }))
+          };
+          o[k] = r;
+        }
+      }
+    }
+    if (getMyResources) {
+      o = { ...o, ...(await getResources({ cmd, path: currentPath })) };
+    }
   }
+  process.chdir(oldPath);
   return o;
 };
 const writeConfig = (
@@ -23,12 +57,13 @@ const writeConfig = (
   toPath = process.cwd(),
   fileName = "config.json"
 ) => {
-  const fullPath = Path.join(toPath, fileName);
-  fs.writeFileSync(fullPath, JSON.stringify(fromObject));
+  const fullPath = join(toPath, fileName);
+  writeFileSync(fullPath, JSON.stringify(fromObject, null, 2));
   return true;
 };
 module.exports = {
   writeConfig,
   getAccountID,
-  makeConfig
+  makeConfig,
+  getResources
 };
